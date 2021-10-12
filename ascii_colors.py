4e37443bc64d51131f1ba61a764377ef3b9a006e
Paul Coste <paul@lescoste.net>
2021-10-12 16:30:46 +0000

    v1 de colors

--- ascii_colors.py
+++ ascii_colors.py
@@ -3,10 +3,14 @@ from PIL import Image
 import math
 import numpy as np
 import webcolors
+import matplotlib
 
-# ascii characters
+# Global variables
 ASCII_CHARS = ["@", "#", "S", "%", "?", "*", "+", ";", ":", ",", "."]
 N = len(ASCII_CHARS)
+list_of_colors = ['grey','red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white']
+list_of_rgb = [[128,128,128],[255,0,0],[0,255,0],[255,255,0],[0,0,255],[255,0,255],[0,255,255],[255,255,255]]
+number_of_colors = len(list_of_colors)
 
 # resize image
 def resize_image(image, new_width):
@@ -20,31 +24,25 @@ def resize_image(image, new_width):
 def grayify(image):
     return image.convert("L")
 
-def closest_colour(requested_colour):
-    min_colours = {}
-    for key, name in webcolors.css3_hex_to_names.items():
-        r_c, g_c, b_c = webcolors.hex_to_rgb(key)
-        rd = (r_c - requested_colour[0]) ** 2
-        gd = (g_c - requested_colour[1]) ** 2
-        bd = (b_c - requested_colour[2]) ** 2
-        min_colours[(rd + gd + bd)] = name
-    return min_colours[min(min_colours.keys())]
-
-def get_colour_name(requested_colour):
-    try:
-        closest_name = actual_name = webcolors.rgb_to_name(requested_colour)
-    except ValueError:
-        closest_name = closest_colour(requested_colour)
-        actual_name = None
-    return actual_name, closest_name
+# get closest color for the pixel in list_of_colors
+def get_closest_color(pixel):
+    distances = []
+    for number in range(number_of_colors):
+        distance = 0
+        for i in range(3):
+            distance += (pixel[i]-list_of_rgb[number][i])**2        
+        distances.append(distance)
+    return list_of_colors[np.argmin(np.array(distances))]
 
 # stores pixels color info
-def pixels_to_color_name(pixels):
+def pixels_to_color_name(image):
+    pixels = image.getdata()
     color_info = []
     for pixel in pixels:
-        color_info.append(webcolors.rgb_to_name(pixel))
+        color_info.append(get_closest_color(pixel))
     return np.array(color_info)
 
+# converts a shade of grey image into a string of ascii characters
 def pixels_to_ascii(image):
     pixels = image.getdata()
     characters = ""
@@ -59,19 +57,36 @@ def main():
         image = PIL.Image.open(path)
     except:
         print(path, "is not a valid pathname to an image")
+
     resized_image = resize_image(image,new_width)
     
-    grey_image = grayify(image)
-    color_info = pixels_to_color_name(image)
+    grey_image = grayify(resized_image)
+    color_info = pixels_to_color_name(resized_image)
 
     new_image_data = pixels_to_ascii(grey_image)
     for i in range(len(new_image_data)):
         if new_image_data[i] == ".":
             new_image_data = new_image_data[:i]+" "+new_image_data[i+1:]
     pixel_count = len(new_image_data)
-    ascii_image = "\n".join([new_image_data[i:(i+new_width)] for i in range(0,pixel_count, new_width)])
+    added_length = len("\033[1;30;40m\033[0m 1;30;40m")+1
+    for i in range(len(new_image_data)):
+        for color_index in range(number_of_colors):
+            index = color_index+30
+            if color_info[i] == list_of_colors[color_index]:
+                new_image_data = new_image_data[:i]
+                new_image_data+="\033[1;"+str(index)+";40m"+new_image_data[i]
+                new_image_data+="\033[0m 1;"+str(index)+";40m"+new_image_data[i+1:]
+                break
+    ascii_image = "\n".join([new_image_data[i:(i+new_width)] for i in range(0,pixel_count, added_length*new_width)])
     print(ascii_image)
-        
-# main()
 
-# print(webcolors.rgb_to_name([255,0,0]))
+# main()
+# ['grey','red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white']
+# print("\033[1;30;40m Gray    \033[0m 1;30;40m")
+# print("\033[1;31;40m Red     \033[0m 1;31;40m")
+# print("\033[1;32;40m Green   \033[0m 1;32;40m")
+# print("\033[1;33;40m Yellow  \033[0m 1;33;40m")
+# print("\033[1;34;40m Blue    \033[0m 1;34;40m")
+# print("\033[1;35;40m Magenta \033[0m 1;35;40m")
+# print("\033[1;36;40m Cyan    \033[0m 1;36;40m")
+# print("\033[1;37;40m White   \033[0m 1;37;40m")
