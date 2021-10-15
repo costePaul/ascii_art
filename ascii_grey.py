f3133d27b92165303090a78f5041b34ce1ac73d4
Paul Coste <paul@lescoste.net>
2021-10-15 11:29:52 +0000

    Changing ouput location for ascii_grey

--- ascii_grey.py
+++ ascii_grey.py
@@ -18,12 +18,28 @@ def resize_image(image, new_width):
 def grayify(image):
     return image.convert("L")
 
+# converts a shade of grey image into a string of ascii characters
 def pixels_to_ascii(image):
     pixels = image.getdata()
     characters = ""
     for pixel in pixels:
         characters += ASCII_CHARS[pixel//math.floor(255/(N-1))]
     return characters
+
+def get_image_directory_and_name(path):
+    last_slash_index = 0
+    last_dot_index = 0
+    n = len(path)
+    for i in range(n):
+        if path[i]=='/':
+            last_slash_index = i
+    for i in range(last_slash_index,n):
+        if path[i]=='.':
+            last_dot_index = i
+    if last_dot_index==0:
+        return path[:last_slash_index],path
+    else :
+        return path[:last_slash_index],path[last_slash_index+1:last_dot_index]
 
 def main():
     new_width = int(input("Enter a new width for the output:\n"))
@@ -38,6 +54,11 @@ def main():
             new_image_data = new_image_data[:i]+" "+new_image_data[i+1:]
     pixel_count = len(new_image_data)
     ascii_image = "\n".join([new_image_data[i:(i+new_width)] for i in range(0,pixel_count, new_width)])
-    print(ascii_image)
-        
+    
+    # print(ascii_image)
+    dir,name = get_image_directory_and_name(path)
+    result_path = dir+'/'+name+'.txt'
+    with open(result_path, 'w') as f:
+                f.write(ascii_image)
+
 main()
